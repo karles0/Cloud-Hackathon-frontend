4ae28e13a800f09af14acfcfdf601dd56234650e
Franco Gonzalez <franco.gonzalez@utec.edu.pe>
2026-06-20 16:17:04 -0500

    Frontend inicial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-50 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-slate-800">
+        Centinela de Integridad Científica
+      </h1>
+    </div>
+  )
+}
+
+export default App
